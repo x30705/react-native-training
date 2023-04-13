291d13ccbbcc0428b099691d744f72e4f700efec
Gerardo Razo Jabana <x30705@gmail.com>
2023-04-13 08:16:40 -0600

    Add Counter.

--- MyApp/src/screens/CounterScreen.tsx
+++ MyApp/src/screens/CounterScreen.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import { Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Text, View, Button } from 'react-native';
 
 export const CounterScreen = () => {
 
-  return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#32de84',
-      justifyContent: 'center',
-    }}>
-      <Text style={{
-        fontSize: 45,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        top: -200,
-      }}>
-        Contador: 0
-      </Text>
-    </View>
-  )
+    // Componentes de React Native: https://reactnative.dev/docs/components-and-apis
+
+    const [counter, setCounter] = useState(5);
+
+    return (
+        <View style={{
+        flex: 1,
+        backgroundColor: '#32de84',
+        justifyContent: 'center',
+        }}>
+        <Text style={{
+            fontSize: 45,
+            textAlign: 'center',
+            fontWeight: 'bold',
+            top: -200,
+        }}>
+            Contador: {counter}
+        </Text>
+        <Button
+            onPress={() => setCounter(counter + 1)}
+            title="Learn More"
+            color="#841584"
+        />
+        </View>
+    )
 }
